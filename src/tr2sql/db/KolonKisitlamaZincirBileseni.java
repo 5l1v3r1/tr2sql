89af62e61116b625249d2d3104d96595f579cc36
ahmetaa <ahmetaa@7153fe06-2a43-0410-9ce2-c73cf035c932>
2008-03-17 04:51:40 +0000

    dagitim mekanizmasi eklendi.

--- src/tr2sql/db/KolonKisitlamaZincirBileseni.java
+++ src/tr2sql/db/KolonKisitlamaZincirBileseni.java
@@ -1,6 +1,9 @@
 package tr2sql.db;
 
 
+/**
+ * Bu sinif iki kolon kisitlama bileseninin nasil bir kosul ile baglandigini gosterir. bilesen1 AND bilesen2 gibi.
+ */
 public class KolonKisitlamaZincirBileseni {
     public KolonKisitlamaBileseni kisitlamaBileseni;
     public KosulTipi sonrakiBilsenIliskisi;
